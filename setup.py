9721a870705d1fa4157e8bd8d643a0247e03066a
Frank Natividad <contact@franknatividad.com>
2015-10-05 23:19:35 -0600

    Changes * Incremented delay time to a minutes (digital ocean has an issue with lesser than that.)

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class ElasticPowerTAC:
 					actions_all_completed = False
 			if not actions_all_completed:
 				# If not finished sleep for 1 minute
-				time.sleep(30)
+				time.sleep(60)
 			else:
 				break
 
